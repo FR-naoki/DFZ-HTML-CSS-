474e7aa475a2e1633febc01b203500776e79a7fc
FR <naoki.maekawa.511@gmail.com>
2024-01-16 17:02:21 +0900

    1/16 84-86

--- html/question.php
+++ html/question.php
@@ -1,26 +1,34 @@
 <?php   
 
-$id = '1';
-$question = 'HTMLはなんの略称？';
+require __DIR__.'/../lib/functions.php';
 
-// <li data-answer="A">HyperTextMakingLanguage</li>
-// <li data-answer="B">HyperTextMarkupLanguage</li>
-// <li data-answer="C">HonmaniTensaitekinaMajidesugoiLanguage</li>
-// <li data-answer="D">そもそも略称ではない</li>
+$id = '3';
+
+$data = fetchById($id);
+// echo '<pre>';
+// var_dump($data);
+
+// $handler = fopen(__DIR__.'/../lib/data.csv', 'r');
+// $one = fgetcsv($handler);
+// $two = fgetcsv($handler);
+// $three = fgetcsv($handler);
+
+// var_dump($one);
+// var_dump($two);
+// var_dump($three);
+
+$question = nl2br(htmlspecialchars($data[1]));
 
 $answers = [
-    'A' => 'HyperTextMakingLanguage', 
-    'B' => 'HyperTextMarkupLanguage', 
-    'C' => 'HonmaniTensaitekinaMajidesugoiLanguage', 
-    'D' => 'そもそも略称ではない',
+    'A' => htmlspecialchars($data[2]),
+    'B' => htmlspecialchars($data[3]),
+    'C' => htmlspecialchars($data[4]),
+    'D' => htmlspecialchars($data[5]),
 ];
 
-// <span id="correct-answer">B. HyperTextMarkupLanguage</span><br>
-// これが間違えてたら「HTMLとは？」の動画を復習お願いします！
-
-$correctAnswer = 'B';
+$correctAnswer = htmlspecialchars(strtoupper($data[6]));
 $correctAnswerValue = $answers[$correctAnswer];
-$explanation = 'これが間違えてたら「HTMLとは？」の動画を復習お願いします！';
+$explanation = nl2br(htmlspecialchars($data[7]));
 
 
 include __DIR__.'/../template/question.tpl.php';
